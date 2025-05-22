29210ddf8538bf1149ec108f71154c88d1e35cad
Md-Sifat-code <codewithsifat4@gmail.com>
2025-05-22 15:04:29 +0600

    new topic

--- src/Select/Topic1.jsx
+++ src/Select/Topic1.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function Topic1() {
   return (
     <div>
-        <h1>Topic1</h1>
-      
+      <h1>Topic1</h1>
+      <p>new</p>
     </div>
-  )
+  );
 }
 
-export default Topic1
+export default Topic1;
